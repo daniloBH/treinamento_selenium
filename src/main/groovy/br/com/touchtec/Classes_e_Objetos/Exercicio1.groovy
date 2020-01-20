5ec8f7e5465a927d7c49124b4fc9a51b76a7165c
Danilo Hernandes <danilohernandes@touchtec.com.br>
2020-01-20 16:48:59 -0300

    Aula de Classes e Objetos
    Change-Id: I11c0d92da22dda088a9ea3006dcc78fb5c360925

---
+++ src/main/groovy/br/com/touchtec/Classes_e_Objetos/Exercicio1.groovy
@@ -0,0 +1,16 @@
+package br.com.touchtec.Classes_e_Objetos
+
+
+/**
+ * Construindo a sua casa
+ * Crie uma classe chamada CasaDosSonhos que tenha os seguintes atributos:
+ * 1) número de comodos
+ * 2) tamanho do terreno em metros quadrados
+ * 3) texto descrevendo aonde a casa ficaria
+ * 4) tem piscina?
+ * 5) tamanho do jardim em metros quadrados
+ * 6) um texto com alguma característica importante pra sua casa dos sonhos
+ *
+ * Depois de criada a classe, crie o objeto da sua Casa dos Sonhos com os valores que
+ * sua casa dos sonhos teria
+ */
--- src/main/groovy/br/com/touchtec/Funções/FuncoesExemplo.groovy
+++ src/main/groovy/br/com/touchtec/Funções/FuncoesExemplo.groovy
@@ -10,15 +10,28 @@ def mostra = { def texto ->
     return answer
 }
 
+Closure<Integer> pedeInteiro = {
+    JFrame frame = new JFrame()
+    String answer = JOptionPane.showInputDialog(frame, "Digite um inteiro")
+    frame.dispose()
+    return Integer.parseInt(answer)
+}
+
+def mostraLista = { List<String> lista ->
+    mostra(lista.join(", "));
+}
+
 /** Funções
  *  (Também chamadas de Métodos no Java)
  *  O que é? Um pedaço de código/lógica que tem um nome que você chama
  *  Como cria uma? Você declara a função definindo o nome, o que ela retorna e os parâmetros dela
  */
 
-String olaPessoa(String nome) {
-    return "Olá " + nome
+String olaPessoa(String nome, Integer idade) {
+    return("Olá " + nome + ", Idade:" + idade)
 }
+
+//olaPessoa("Paulo", 18)
 
 /**
  * O nome da função é como ela será identificada, no exemplo acima o nome dela é: olaPessoa
@@ -37,6 +50,7 @@ void oiMundo() {
     print("Olá Mundo!")
 }
 
+oiMundo()
 /**
  * Parâmetros são o que passamos pra função para ela fazer a função dela (piadão bom aqui)
  */
@@ -49,6 +63,21 @@ Integer determinaMaior(Integer a, Integer b) {
     }
 }
 
+//List<String> lista = []
+//lista.add(pedeInteiro().toString())
+//lista.add(pedeInteiro().toString())
+//mostraLista(lista)
+
+void mostraMaior(Integer a, Integer b) {
+    if(a > b) {
+        println(a)
+    }
+    else {
+        println(b)
+    }
+}
+
+mostraMaior(5, 6)
  /**
  * Chamar a função é que nem chamar alguém, você usa o nome da criatura.
  */
