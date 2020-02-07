47f41e73dcf6f012dd98ac101998e1a4f0d4375b
Danilo Hernandes <danilohernandes@touchtec.com.br>
2020-02-07 15:43:32 -0300

    Aula de Hierarquias
    Exercicios e aulas adicionados
    
    Change-Id: I534d9ccbf896c3bdc82fca41156e655a73d86cfb

---
+++ src/main/groovy/br/com/touchtec/Hierarquia/Exercicio1.groovy
@@ -0,0 +1,71 @@
+package br.com.touchtec.Hierarquia
+
+import javax.swing.JFrame
+import javax.swing.JOptionPane
+
+/**
+ * Nesse exercicio temos a classe abstrata Bicho que possui um conjunto de propriedades e o método abstrato getInfoFicha.
+ *
+ * Você deve criar 3 classes de animais diferentes que extendam Bicho e implementem o getInfoFicha de jeitos diferentes.
+ * A implementação do getInfoFicha pode ser só utilizar propriedades diferentes para montar o String que devolve. Por exemplo,
+ * podemos juntar o nomeComum, nome Cientifico e a descricao para montar um único String que será devolvido pelo getInfoFicha.
+ *
+ * Depois de criar essas classes, crie objetos das classes que você fez e crie um objeto da classe FichaDosBichos,
+ * depois use os métodos da FichaDosBichos pra exibir as informações de cada bicho.
+ */
+
+abstract class Bicho {
+    String nomeComum
+    String nomeCientifico
+    String descricao
+    String habitat
+
+    abstract String getInfoFicha()
+
+    String getNomeComum() {
+        return nomeComum
+    }
+
+    void setNomeComum(String nomeComum) {
+        this.nomeComum = nomeComum
+    }
+
+    String getNomeCientifico() {
+        return nomeCientifico
+    }
+
+    void setNomeCientifico(String nomeCientifico) {
+        this.nomeCientifico = nomeCientifico
+    }
+
+    String getDescricao() {
+        return descricao
+    }
+
+    void setDescricao(String descricao) {
+        this.descricao = descricao
+    }
+
+    String getHabitat() {
+        return habitat
+    }
+
+    void setHabitat(String habitat) {
+        this.habitat = habitat
+    }
+}
+
+class FichaDosBichos {
+    String informacao
+
+    String infoBicho(Bicho bicho) {
+        this.informacao = bicho.getInfoFicha()
+    }
+
+    String mostraInformacao(String texto) {
+        JFrame frame = new JFrame()
+        String answer = JOptionPane.showMessageDialog(frame, texto.toString())
+        frame.dispose()
+        return answer
+    }
+}
