d39cb23d55d3003537b96beb1f13c05dd130584e
Danilo Hernandes <danilohernandes@touchtec.com.br>
2020-02-21 10:19:20 -0300

    Arrumando nome do diretório
    Change-Id: Icff70c15a5822507297c8050f52be91d53d9e0ad

--- src/main/groovy/br/com/touchtec/Funcoes/Exercicio1.groovy
+++ src/main/groovy/br/com/touchtec/Funcoes/Exercicio1.groovy
@@ -1,4 +1,4 @@
-package br.com.touchtec.Funções
+package br.com.touchtec.Funcoes
 
 import javax.swing.JFrame
 import javax.swing.JOptionPane
--- src/main/groovy/br/com/touchtec/Funcoes/Exercicio3.groovy
+++ src/main/groovy/br/com/touchtec/Funcoes/Exercicio3.groovy
@@ -1,4 +1,4 @@
-package br.com.touchtec.Funções
+package br.com.touchtec.Funcoes
 
 
 /**
