23645a775ba7953ffe2f9c61fd18ab5a78927528
kathleenmiranda <kathleenmiranda@touchtec.com.br>
2020-08-11 16:01:15 -0300

    Private

--- src/main/groovy/br/com/touchtec/Modificadores_de_Acesso/Exercicio.groovy
+++ src/main/groovy/br/com/touchtec/Modificadores_de_Acesso/Exercicio.groovy
@@ -10,17 +10,31 @@ class PastaDeArquivos {
     private String nomePasta
     private List<String> conteudoArquivos
 
-    PastaDeArquivos(Long idDiretorio, String nomePasta, List<String> conteudoArquivos, List<Integer> arquivosPrivados) {
+    PastaDeArquivos(Long idDiretorio, String nomePasta, List<String> conteudoArquivos) {
         this.idDiretorio = idDiretorio
         this.nomePasta = nomePasta
         this.conteudoArquivos = conteudoArquivos
+    }
+
+    Long getIdDiretorio(){
+        return idDiretorio
+    }
+    void setIdDiretorio(Long idDiretorio){
+        this.idDiretorio = idDiretorio;
+    }
+
+    String getNomePasta(){
+        return nomePasta;
+    }
+    void setNomePasta(String nomePasta){
+        this.nomePasta = nomePasta;
     }
 
     private alterarIdDiretorio(Long novoId) {
         this.idDiretorio = novoId
     }
 
-    void alterarNomePasta(String novoNome){
+    void alterarNomePasta(String novoNome) {
         this.nomePasta = novoNome
     }
 
@@ -28,25 +42,28 @@ class PastaDeArquivos {
         this.conteudoArquivos = novoConteudo
     }
 
+
     void mostrarInfosPasta() {
-        println("Id:" + this.idDiretorio+ "\n" +
+        println("Id:" + this.idDiretorio + "\n" +
                 "Nome da Pasta:" + this.nomePasta + "\n" +
                 "Conteudo Arquivos:" + this.conteudoArquivos + "\n")
     }
 
-
-
 }
-
 List<String> conteudoDocs = ["Relatórios Gerência", "Documentação Issues Novas", "Folha de Pagamento"]
+List<String> conteudo = ["Relatórios Gerência - 2019", "Documentação Issues 2019", "Folha de Pagamento - 2019"]
 
-PastaDeArquivos docsTouch = new PastaDeArquivos(532, "docsTouch", conteudoDocs, [2])
+PastaDeArquivos docsTouch = new PastaDeArquivos(532, "docsTouch", conteudo);
+docsTouch.setIdDiretorio(588);
+docsTouch.setNomePasta("docsTouch2019");
+//conteudoDocs+=["Relatórios Gerência - 2019"]
+
 
 /**
- * Esses debvem ser os valores do docsTouch a serem mostrados
+ * Esses devem ser os valores do docsTouch a serem mostrados
  * Id: 588
  * Nome da Pasta: docsTouch2019
  * Conteudo Arquivos: ["Relatórios Gerência - 2019", "Documentação Issues 2019", "Folha de Pagamento - 2019"]
  */
 
-docsTouch.mostrarInfosPasta()
+docsTouch.mostrarInfosPasta();
